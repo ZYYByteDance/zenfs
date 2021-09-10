bd97ceca6058eb8381bacee059bda2ba6f4d680a
Zhengyu Yang <zhengyu.yang@bytedance.com>
2021-09-10 22:37:02 +0000

    Updated WAF.sh.

--- util/WAF.sh
+++ util/WAF.sh
@@ -8,7 +8,7 @@ sudo ../../../output/zenfs mkfs --aux_path=/tmp/zenfs_nvme3n2 --zbd=nvme3n2 --fo
 sudo nvme smart-log /dev/nvme3n2 > SMART-Before.txt
 
 # run test
-sudo sh Config.sh
+sudo sh WAFTestConfig.sh
 #sudo ../../../output/db_bench "$(< Config.txt)"
 # ../../../output/zenfs_test --zbd=nvme3n2
 
