942f9d72773cef6ed848c164ec1f800417411ff5
Zhengyu Yang <zhengyu.yang@bytedance.com>
2021-09-10 03:30:27 +0000

    Added terarkDB config file and WAF script with real WAF calculation.

--- util/WAF.py
+++ util/WAF.py
@@ -1,32 +1,83 @@
 #!/usr/bin/env python3
 
 
-infile1 = open("before.txt", "r")
-infile2 = open("after.txt",  "r")
-outfile = open("Output.txt", "w")
+# open files
+configFile = open("Config.txt", "r")
+beforeFile = open("SMART-Before.txt", "r")
+afterFile  = open("SMART-After.txt",  "r")
+resultFile = open("Output.txt", "w")
+
+		
+		
+def getLogicalWriteAmount(infile):
+	key=0
+	value=0
+	thread=0
+	num=0
+	strList=[]
+	for line in infile:
+		curLineStrings=line.split(" ")
+		#print(curLineStrings)
+		for string in curLineStrings:
+			if ("--key_size" in string):
+				#print(string)
+				key=int(string.split("=")[1])
+				#print(key)
+			if ("--value_size" in string):
+				#print(string)
+				value=int(string.split("=")[1])
+				#print(value)			
+			if ("--threads" in string):
+				#print(string)
+				thread=int(string.split("=")[1])
+				#print(thread)
+			if ("--num=" in string):
+				#print(string)
+				num=int(string.split("=")[1])
+				#print(num)
+	if ((key+value)*thread*num>0):
+		return (key+value)*thread*num/1024.0
+	else:
+		return -1
+	
 
 
-
-def findWriteNum(infile):
+def getPhysicalWriteAmount(infile):
 	for line in infile:
 		curLine=line.split(":")
 		if (curLine[0]=="data_units_written			"):
 			writeNum =int(curLine[1].replace('\n','').replace(',', ''))
-			print("writeNum=", writeNum)
-			return writeNum
+			print(writeNum/2.0)
+			return writeNum/2.0
 	return -1
 			
+
 	
-before=findWriteNum(infile1)
-after=findWriteNum(infile2)
-if (before==-1 or after==-1):
+# main process
+
+# get logical write amount in KB
+logWriteAmount=getLogicalWriteAmount(configFile)
+
+# get physical write amount in KB
+writeCountBefore=getPhysicalWriteAmount(beforeFile)
+writeCountAfter =getPhysicalWriteAmount(afterFile)
+
+if (writeCountBefore==-1 or writeCountAfter==-1):
 	print("Error")
+	exit()
 else:
-	delta=(after-before)/2.0
-	print(delta, "KB")
-	printLine="delta="+ str(delta)+"KB"
-	outfile.write(printLine)
-	
-infile1.close()
-infile2.close()
-outfile.close()
+	delta=writeCountAfter-writeCountBefore # *512KB
+	if(logWriteAmount==0):
+		WAF=0
+	else:
+		WAF=delta/logWriteAmount
+	printLine="logWrite="+str(logWriteAmount)+"KB, phyWrite="+ str(delta) + "KB, WAF=" + str(WAF)
+	print(printLine)
+	resultFile.write(printLine)
+
+
+# close files
+configFile.close()
+beforeFile.close()
+afterFile.close()
+resultFile.close()
--- util/WAF.sh
+++ util/WAF.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo nvme smart-log /dev/nvme3n2 > before.txt
-sudo ../../../output/zenfs_test --zbd=nvme3n2
-sudo nvme smart-log /dev/nvme3n2 > after.txt
-sudo WAF.py
+sudo nvme smart-log /dev/nvme3n2 > SMART-Before.txt
+sudo ../../../output/db_bench "$(< Config.txt)"
+# ../../../output/zenfs_test --zbd=nvme3n2
+sudo nvme smart-log /dev/nvme3n2 > SMART-After.txt
+python WAF.py
